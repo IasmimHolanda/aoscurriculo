315e12430b46a6fff8329f35f9cd09b2a04d5e49
IasmimHolanda <106707727+IasmimHolanda@users.noreply.github.com>
2023-12-06 13:11:15 -0300

    Add files via upload

---
+++ iasmim/src/main/java/com/example/iasmim/IasmimApplication.java
@@ -0,0 +1,13 @@
+package com.example.iasmim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasmimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IasmimApplication.class, args);
+	}
+
+}
---
+++ iasmim/src/test/java/com/example/iasmim/IasmimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iasmim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasmimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
